43bc3880f5e8a7ae7c0014f8a8390211726f27fb
ihsan10 <ihsansolangi225@gmail.com>
2022-04-20 06:06:59 +0500

    Update 2.0
    Fixed input validation errors with 'try' and 'except' statements.

--- collatz.py
+++ collatz.py
@@ -1,14 +1,29 @@
-print('Please enter your number.')
+print('Collatz Sequence')
 
 def collatz(number):
 
-    while number != 1:
-        if number % 2 == 0:
-            print(str(number // 2))
-            number = number // 2
-        else:
-            print(str(3 * number + 1))
-            number = 3 * number + 1
+    if number % 2 == 0:
+        print(number // 2)
+        return number // 2
+    else:
+        print(3 * number + 1)
+        return 3 * number + 1
+
+while True:
+    
+    try:
+        user_input = int(input('Please enter a number: '))
+        
+        if user_input <= 1:
+            print('Try any number bigger than 1')
+            
+        while user_input > 1:
+            user_input = collatz(user_input)
+        
+    except ValueError:
+        print('Your input must be a number!')
+        
+    break
+
 
         
-collatz(int(input()))
